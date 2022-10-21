d8b50ac9c06598a89cfa7f6636ceb480d1afcce4
DmitryDutov <dmitry_dutov@inbox.ru>
2022-10-21 23:11:27 +0300

    Перенёс упражнение 1 в класс

---
+++ TestConsole/Exercise001.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class Exercise001
+    {
+        public void Start()
+        {
+            var path = @"C:\Users\ThinkPad\Downloads\olimpiada2022_task2_2_3.txt";
+
+            var text = File.ReadAllText(path);
+            var list = text.Split('\n');
+
+            int num = 1;
+            var listValueTypel = new List<ValueTuple<int, int, int, string>>();
+            var maxNum = 0;
+
+            foreach (var item in list)
+            {
+                var vloj = item.Split('\\').ToList().Count - 1;
+                var name = item.Split('\\').LastOrDefault();
+                var lenght = item.Length;
+                Console.WriteLine("{0}; {1}; {2}; {3}", lenght, num, vloj, name);
+
+                if (vloj > maxNum)
+                {
+                    maxNum = vloj;
+                }
+
+                var obj = (lenght, num, vloj, name);
+                listValueTypel.Add(obj);
+
+                num++;
+            }
+
+            var result = listValueTypel.Select(x => x).Where(x => x.Item3 == maxNum).ToList().FirstOrDefault();
+            Console.WriteLine($"Result: {result.Item4} {result.Item3}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,32 +1,4 @@
-﻿var path = @"C:\Users\ThinkPad\Downloads\olimpiada2022_task2_2_2.txt";
+﻿using TestConsole;
 
-var text = File.ReadAllText(path);
-var list = text.Split('\n');
-
-int num = 1;
-var listValueTypel = new List<ValueTuple<int, int, int, string>>();
-var maxNum = 0;
-
-foreach (var item in list)
-{
-    var vloj = item.Split('\\').ToList().Count - 1;
-    var name = item.Split('\\').LastOrDefault();
-    var lenght = item.Length;
-    Console.WriteLine("{0}; {1}; {2}; {3}", lenght, num, vloj, name);
-
-    if (vloj>maxNum)
-    {
-        maxNum = vloj;
-    }
-
-    var obj = (lenght, num, vloj, name);
-    listValueTypel.Add(obj);
-
-    num++;
-}
-
-var result = listValueTypel.Select(x => x).Where(x => x.Item3 == maxNum).ToList().FirstOrDefault();
-Console.WriteLine($"Result: {result.Item3} {result.Item4}");
-
-Console.ReadLine();
-
+Exercise001 ex = new Exercise001();
+ex.Start();

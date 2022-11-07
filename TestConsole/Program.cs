899f70e5c8590dae958794b070c2609eef9207af
DmitryDutov <dmitry_dutov@inbox.ru>
2022-11-07 14:15:09 +0300

    Разные способы чтения

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -3,6 +3,9 @@
 //Exercise004 ex = new Exercise004();
 //ex.Start();
 
-CreateGuid create = new CreateGuid();
-create.Start();
+//CreateGuid create = new CreateGuid();
+//create.Start();
+
+Test test = new Test();
+test.TextReadAsync();
 
---
+++ TestConsole/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class Test
+    {
+        public async Task TextReadAsync()
+        {
+            var path = @"C:\Users\ThinkPad\Downloads\!License-8.sll";
+            var reader = new BinaryReader(File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read));
+            var text = File.ReadAllText(path);
+
+            using (FileStream fstream = File.OpenRead(path))
+            {
+                // выделяем массив для считывания данных из файла
+                byte[] buffer = new byte[fstream.Length];
+                // считываем данные
+                await fstream.ReadAsync(buffer, 0, buffer.Length);
+                // декодируем байты в строку
+                string textFromFile = Encoding.Default.GetString(buffer);
+                Console.WriteLine($"Текст из файла: {textFromFile}");
+            }
+
+            Console.WriteLine(reader.ReadString());
+            Console.ReadLine();
+        }
+    }
+}
+
